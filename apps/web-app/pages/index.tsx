4ca74f44d6d60ba04f6517cc3305bbdeb4d41347
Daniel Sogl <daniel@sogls.de>
2022-05-07 18:55:07 +0200

    refactor(web-app): wrap index view with accordion

--- apps/web-app/pages/index.tsx
+++ apps/web-app/pages/index.tsx
@@ -1,13 +1,21 @@
+import { Accordion } from '@mantine/core';
 import { useState } from 'react';
 import CsvDropzone from '../components/csv-dropzone/csv-dropzone';
 import { CakeDeFiTransaction } from '../models/cake-transaction.model';
 
 export function Index() {
-  const [transactions, setTransactions] = useState<CakeDeFiTransaction[]>([]);
+  const [transactions, setTransactions] =
+    useState<CakeDeFiTransaction[]>(undefined);
 
   return (
     <>
-      <CsvDropzone transactionParsed={setTransactions} />
+      <Accordion multiple initialItem={0}>
+        <Accordion.Item label="Transactions CSV">
+          <CsvDropzone transactionParsed={setTransactions} />
+        </Accordion.Item>
+        <Accordion.Item label="Transactions"></Accordion.Item>
+        <Accordion.Item label="Transactions Evaluation"></Accordion.Item>
+      </Accordion>
     </>
   );
 }
